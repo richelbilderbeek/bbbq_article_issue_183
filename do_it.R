50c60eaeee29f6212e4fa982dcf5b5cdf8051588
richelbilderbeek <richel@richelbilderbeek.nl>
2021-02-18 11:24:44 +0100

    Do first and last five positions

--- do_it.R
+++ do_it.R
@@ -14,7 +14,7 @@ library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
 #'   '-1' means the last character, '-2' the one-but-last
 analyse_epitopes <- function(
   epitope_sequences,
-  positions = c(1, 2, 3, -3, -2, -1)
+  positions = c(1, 2, 3, 4, 5, -5, -4, -3, -2, -1)
 ) {
   testthat::expect_true(is.character(epitope_sequences))
   testthat::expect_true(is.numeric(positions))
@@ -29,6 +29,7 @@ analyse_epitopes <- function(
   # Count all chararacters at all positions
   for (i in seq_along(positions)) {
     pos <- positions[i]
+    testthat::expect_true(all(nchar(char_tibble$epitope_sequence) >= abs(pos)))
     char_tibble$char <- stringr::str_sub(char_tibble$epitope_sequence, pos, pos)
     t <- dplyr::count(char_tibble, char)
     t$pos <- pos
@@ -44,7 +45,7 @@ analyse_epitopes <- function(
   # Make the column names reader friendly
   names(t_wide) <- c(
     names(t_wide)[1],
-    stringr::str_replace(english::as.english(as.numeric(names(t_wide)[2:7])), " ", "_")
+    stringr::str_replace(english::as.english(as.numeric(names(t_wide)[-1])), " ", "_")
   )
 
   return(t_wide)
@@ -53,7 +54,7 @@ analyse_epitopes <- function(
 t_all_unique_matches <- readr::read_csv("matches_1.csv") %>% filter(n_matches == 1)
 
 png("all_presented_epitope_lengths.png")
-hist(nchar(t_all_unique_matches$epitope_sequence), freq = TRUE, breaks = seq(6, 25), main = "Presented MHC-I epitope lengths")
+hist(nchar(t_all_unique_matches$epitope_sequence), freq = TRUE, main = "Presented MHC-I epitope lengths")
 dev.off()
 
 t_wide <- analyse_epitopes(epitope_sequences = t_all_unique_matches$epitope_sequence)
@@ -66,7 +67,7 @@ readr::write_csv(t_wide, "all.csv")
 
 t_unique_tmhs_tmhmm_1 <- readr::read_csv("tmhs_tmhmm_1.csv") %>% filter(n_matches == 1)
 png("tmhs_presented_epitope_lengths.png")
-hist(nchar(t_unique_tmhs_tmhmm_1$epitope_sequence), freq = TRUE, breaks = seq(7, 24), main = "Presented MHC-I TMH epitopes' lengths")
+hist(nchar(t_unique_tmhs_tmhmm_1$epitope_sequence), freq = TRUE, main = "Presented MHC-I TMH epitopes' lengths")
 dev.off()
 
 
